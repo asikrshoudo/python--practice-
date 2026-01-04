18d762c355dfbb8de62c7a490198f993cbf996c3
asikrshoudo <shoudoashikurshek@gmail.com>
2026-01-05 03:27:27 +0600

    3rd one ;)

---
+++ verify.py
@@ -0,0 +1,9 @@
+people = ["shoudo","rohan","ruhan","sifat","pranto","zihad","mahim"]
+
+name = input("vai tor nam ki :) ->")
+if name in people:
+   print(name,"kire ki khobor!!")
+else:
+   print(name,"vai ami tomake chini na :(")
+
+
