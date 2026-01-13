21a765bca9589537457d0020886d2f385ed0ebda
asikrshoudo <shoudoashikurshek@gmail.com>
2026-01-14 00:28:10 +0600

    maybe a calculator

---
+++ break_and_continue.py
@@ -0,0 +1,21 @@
+terminate = False
+while not terminate:
+    number1 = input("Please enter a number: ")
+    number1 = int(number1)
+    number2 = input("Please enter another number: ")
+    number2 = int(number2)
+
+    while True:
+        operation = input("Please enter add/sub or  quite to exit: ")
+        if operation == "quite":
+            terminate = True
+            break
+        if operation not in ["add", "sub"]:
+            print("Unknown operation!")
+            continue
+        if operation == "add":
+            print("Result is", number1 + number2)
+            break
+        if operation == "sub":
+            print("Result is", number1 - number2)
+            break
