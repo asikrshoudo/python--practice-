d61f95fc92ba65c90961102c9a77dbb474d865a1
asikrshoudo <shoudoashikurshek@gmail.com>
2026-01-23 02:58:27 +0600

    bro

---
+++ chapter3/leap_year.py
@@ -0,0 +1,10 @@
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("yes")
+        else:
+            print("no")
+    else:
+        print("yes")
+else:
+    print("no")
